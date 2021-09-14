7d9fe67a9ac2c54bb0d1e0bdedcfb21bd6bdc7f8
lenhathuy <huyhuy2899@gmail.com>
2021-09-14 17:24:12 +0700

    TH-21 #ready-for-test implement architecture #time 1h

---
+++ feature/trackhabit/src/main/java/com/track/trackhabit/presentation/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.track.trackhabit.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.track.trackhabit.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
