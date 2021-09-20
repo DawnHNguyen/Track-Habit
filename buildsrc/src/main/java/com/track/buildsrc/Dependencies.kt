f1e9e81d8d4d4c9be15e58856e344b0d220e9373
lenhathuy <huyhuy2899@gmail.com>
2021-09-20 21:55:34 +0700

    TH-21 #ready-for-test update architecture #time 45m

--- buildsrc/src/main/java/com/track/buildsrc/Dependencies.kt
+++ buildsrc/src/main/java/com/track/buildsrc/Dependencies.kt
@@ -1,11 +1,14 @@
 package com.track.buildsrc
 
-object SdkVersion {
-
+object SdkVersions {
+    const val compileSdkVersion = 30
+    const val buildToolsVersion = "30.0.1"
+    const val minSdkVersion = 23
+    const val targetSdkVersion = 30
 }
 
-object Version {
-
+object Versions {
+    val kotlin = "1.5.20"
 }
 
 object Libraries {
@@ -13,15 +16,23 @@ object Libraries {
 }
 
 object KotlinLibraries {
-
+    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
 }
 
 object AndroidLibraries {
 
 }
 
-object Module {
+object Modules {
+    val app = ":app"
 
+    val navigation = ":navigation"
+
+    val common = ":common"
+
+    val remote = ":remote"
+
+    val trackHabit = "::feature:trackhabit"
 }
 
 object TestLibraries{
