78bea70786353703d0c70bd86b2eea6b167bd356
lenhathuy <huyhuy2899@gmail.com>
2021-09-20 16:37:55 +0700

    TH-21 #ready-for-test implement architecture

---
+++ buildsrc/src/main/java/com/track/buildsrc/Dependencies.kt
@@ -0,0 +1,33 @@
+package com.track.buildsrc
+
+object SdkVersion {
+
+}
+
+object Version {
+
+}
+
+object Libraries {
+
+}
+
+object KotlinLibraries {
+
+}
+
+object AndroidLibraries {
+
+}
+
+object Module {
+
+}
+
+object TestLibraries{
+
+}
+
+object Kapt {
+
+}
