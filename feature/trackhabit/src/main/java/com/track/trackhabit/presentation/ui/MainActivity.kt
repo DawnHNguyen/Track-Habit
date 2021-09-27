4b2eb42ac5c517ca231d416abc0109fd2cb92703
lenhathuy <huyhuy2899@gmail.com>
2021-09-27 16:40:28 +0700

    TH-22 update annotation

--- feature/trackhabit/src/main/java/com/track/trackhabit/presentation/ui/MainActivity.kt
+++ feature/trackhabit/src/main/java/com/track/trackhabit/presentation/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.track.trackhabit.presentation.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.track.trackhabit.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
