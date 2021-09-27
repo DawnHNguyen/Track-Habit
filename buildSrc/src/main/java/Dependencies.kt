4bfb94bf71fb1658a2652c3cef6e3aaa55f349f6
lenhathuy <huyhuy2899@gmail.com>
2021-09-27 14:56:49 +0700

    TH-24 #ready-for-test implement room #time 15m

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -14,7 +14,7 @@ object SdkVersions {
 
 object Versions {
     val kotlin = "1.5.31"
-    const val room = "2.2.5"
+    const val room = "2.3.0"
     val timber = "4.7.1"
 
     val appCompat = "1.3.0-alpha01"
