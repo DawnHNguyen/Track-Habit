9f25cfd7a96f15a53b1a3d4776b5dce6a5e6d372
{ninhnau} <vuquangninh321999@gmail.com>
2021-09-29 19:29:01 +0700

    TH-28 #done update database

--- feature/trackhabit/src/main/java/com/track/trackhabit/data/local/UserDatabase.kt
+++ feature/trackhabit/src/main/java/com/track/trackhabit/data/local/UserDatabase.kt
@@ -8,9 +8,11 @@ import androidx.sqlite.db.SupportSQLiteOpenHelper
 import com.track.trackhabit.data.local.dao.HabitDao
 import com.track.trackhabit.data.local.dao.InspectionDao
 import com.track.trackhabit.data.local.dao.UserDao
+import com.track.trackhabit.domain.entity.Habit
+import com.track.trackhabit.domain.entity.Inspection
 import com.track.trackhabit.domain.entity.User
 
-@Database(entities = [User::class],version = 1,exportSchema = false)
+@Database(entities = [User::class, Habit::class, Inspection::class],version = 1,exportSchema = false)
 abstract class UserDatabase: RoomDatabase() {
     abstract fun userDao(): UserDao
     abstract fun habitDao(): HabitDao
