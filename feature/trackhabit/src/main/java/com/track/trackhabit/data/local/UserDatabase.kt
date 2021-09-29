e465a05dc167a107e8c052ac040e329b658d2a1f
{ninhnau} <vuquangninh321999@gmail.com>
2021-09-29 19:25:13 +0700

    TH-28 #tÃest add DAO

--- feature/trackhabit/src/main/java/com/track/trackhabit/data/local/UserDatabase.kt
+++ feature/trackhabit/src/main/java/com/track/trackhabit/data/local/UserDatabase.kt
@@ -1,13 +1,18 @@
-package com.track.trackhabit.data.di.dao
+package com.track.trackhabit.data.local
 
 import androidx.room.Database
 import androidx.room.DatabaseConfiguration
 import androidx.room.InvalidationTracker
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteOpenHelper
+import com.track.trackhabit.data.local.dao.HabitDao
+import com.track.trackhabit.data.local.dao.InspectionDao
+import com.track.trackhabit.data.local.dao.UserDao
 import com.track.trackhabit.domain.entity.User
 
 @Database(entities = [User::class],version = 1,exportSchema = false)
 abstract class UserDatabase: RoomDatabase() {
-
+    abstract fun userDao(): UserDao
+    abstract fun habitDao(): HabitDao
+    abstract fun inspectionDao(): InspectionDao
 }
