2520599bf88b65ccdf3d7d91aeb0fa00f3016234
Dawn Nguyen <lophocpzo@gmail.com>
2021-10-01 15:32:37 +0700

    TH-32 #time 1h creat HabitsListAdapter by copy code from coxude

--- feature/trackhabit/src/main/java/com/track/trackhabit/presentation/ui/HabitsListAdapter.kt
+++ feature/trackhabit/src/main/java/com/track/trackhabit/presentation/ui/HabitsListAdapter.kt
@@ -1,8 +1,11 @@
 package com.track.trackhabit.presentation.ui
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.track.trackhabit.databinding.ItemHabitBinding
 import com.track.trackhabit.domain.entity.Habit
 
 class HabitsListAdapter() :
@@ -20,7 +23,21 @@ class HabitsListAdapter() :
         holder.bind(habit)
     }
 
-    class HabitsListViewHolder private constructor(private val binding: )
+    class HabitsListViewHolder private constructor(private val binding: ItemHabitBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(habit: Habit) {
+            binding.habit = habit
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): HabitsListViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemHabitBinding.inflate(layoutInflater, parent, false)
+                return HabitsListViewHolder(binding)
+            }
+        }
+    }
 
     class HabitsListDiffUtil() : DiffUtil.ItemCallback<Habit>() {
         override fun areContentsTheSame(oldItem: Habit, newItem: Habit) = oldItem == newItem
--- feature/trackhabit/src/main/java/com/track/trackhabit/presentation/ui/MainActivity.kt
+++ feature/trackhabit/src/main/java/com/track/trackhabit/presentation/ui/MainActivity.kt
@@ -2,13 +2,26 @@ package com.track.trackhabit.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.track.trackhabit.R
+import com.track.trackhabit.domain.entity.Habit
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val recyclerView: RecyclerView = findViewById(R.id.test_recyclerView)
+        val habitsListAdapter = HabitsListAdapter()
+        val habitList = mutableListOf<Habit>()
+        val habit = Habit(1, "Ngủ sớm", "", time = Date (12), listOf())
+        habitList.add(habit)
+        habitsListAdapter.submitList(habitList)
+        recyclerView.adapter = habitsListAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
