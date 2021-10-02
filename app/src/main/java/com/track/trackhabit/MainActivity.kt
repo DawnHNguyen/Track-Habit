b27e5cb32d83ab66884a1099c320e5d16fd2c029
Dawn Nguyen <lophocpzo@gmail.com>
2021-10-02 22:18:58 +0700

    Push code for squad 1 to find bug

--- app/src/main/java/com/track/trackhabit/MainActivity.kt
+++ app/src/main/java/com/track/trackhabit/MainActivity.kt
@@ -1,17 +1,13 @@
 package com.track.trackhabit
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.track.trackhabit.presentation.ui.MainActivityFeature
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
-        val button: Button = findViewById(R.id.button_activityMainApp_intent)
-        button.setOnClickListener {
         val intent = Intent(this, MainActivityFeature::class.java)
         startActivity(intent)
         }
