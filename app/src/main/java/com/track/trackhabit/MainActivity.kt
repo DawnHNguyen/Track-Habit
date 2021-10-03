1310a9135fec0e5f55e0854dff99a0b5833995da
Dawn Nguyen <lophocpzo@gmail.com>
2021-10-03 22:42:26 +0700

    TH-32 #ready-for-test #time 3d finish code habit list adapter and fix every bug

--- app/src/main/java/com/track/trackhabit/MainActivity.kt
+++ app/src/main/java/com/track/trackhabit/MainActivity.kt
@@ -4,19 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import com.track.trackhabit.databinding.ActivityMainBinding
 import com.track.trackhabit.habit.presentation.ui.HomeActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         Log.d("check_intent","da goi den")
         val intent = Intent(this, HomeActivity::class.java)
         startActivity(intent)
