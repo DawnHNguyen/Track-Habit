d447015a9d38c7d9618ce8c905c0d673b73b3c64
Nguyen Hoang Dang <lophocpzo@gmail.com>
2021-12-11 22:11:38 +0700

    Add ViewModel and LiveData for sleeptime feature

--- feature/trackhabit/src/main/java/com/track/trackhabit/habit/presentation/ui/sleeptime/SleepTimeViewModel.kt
+++ feature/trackhabit/src/main/java/com/track/trackhabit/habit/presentation/ui/sleeptime/SleepTimeViewModel.kt
@@ -1,5 +1,7 @@
 package com.track.trackhabit.habit.presentation.ui.sleeptime
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.track.common.base.AppDispatchers
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -9,4 +11,32 @@ import javax.inject.Inject
 class SleepTimeViewModel @Inject constructor(
     private val dispatchers: AppDispatchers
 ) : ViewModel() {
+    private val _wakeTime =  MutableLiveData("00:00")
+    val wakeTime: LiveData<String>
+    get() = _wakeTime
+    fun calSleepTime(wakeTime: String, durationHour: Int, durationMin: Int): String {
+        var sleepTime: String
+        val arrayWake = wakeTime.split(':').run {
+            IntArray(size) {
+                get(it).toInt()
+            }
+        }
+        val wakeMin = arrayWake[1]
+        var wakeHour = arrayWake[0]
+
+        val sleepMin: Int
+        if (wakeMin < durationMin) {
+            sleepMin = 60 - (durationMin - wakeMin)
+            wakeHour -= 1
+        } else sleepMin = wakeMin - durationMin
+
+        val sleepHour: Int = if (wakeHour < durationHour) 24 - (durationHour - wakeHour) else wakeHour - durationHour
+        sleepTime = if (sleepHour < 10) "0$sleepHour" else "$sleepHour"
+        sleepTime = if (sleepMin < 10) "$sleepTime:0$sleepMin" else "$sleepTime:$sleepMin"
+        return sleepTime
+    }
+
+    fun setWakeTime(wakeTime: String) {
+        _wakeTime.value = wakeTime
+    }
 }
