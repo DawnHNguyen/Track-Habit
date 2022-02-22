a857f279786431bec3ad75203afeb4de74e9443f
lenhathuy <huyhuy2899@gmail.com>
2022-02-22 15:18:53 +0700

    TH-128 code update habit usecase

--- feature/trackhabit/src/main/java/com/track/trackhabit/habit/presentation/ui/home/EditHabitFragment.kt
+++ feature/trackhabit/src/main/java/com/track/trackhabit/habit/presentation/ui/home/EditHabitFragment.kt
@@ -6,9 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.track.trackhabit.habit.R
 import com.track.trackhabit.habit.databinding.FragmentEditHabitBinding
+import com.track.trackhabit.habit.domain.entity.Habit
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_edit_habit.*
+import timber.log.Timber
+import java.util.*
 
 @AndroidEntryPoint
 class EditHabitFragment: Fragment() {
@@ -34,5 +40,22 @@ class EditHabitFragment: Fragment() {
         editHabitViewModel.getHabit(id)
         binding.textViewFragmentEditHabitDescriptionError.visibility = View.GONE
         binding.textViewFragmentEditHabitNameError.visibility = View.GONE
+        binding.buttonFragmentEditHabitSetTime.text = editHabitViewModel.time.value
+        Timber.d("gia_tri ${editHabitViewModel.time.value}}")
+        Timber.d("gia_tri -${editHabitViewModel.monday.value}}")
+        Timber.d("gia_tri --${editHabitViewModel.tuesday.value}}")
+        Timber.d("gia_tri ---${editHabitViewModel.friday.value}}")
+        binding.buttonFragmentEditHabitButtonDone.setOnClickListener {
+            Timber.d("-->${editHabitViewModel.habit.value!!.time} -- ${id}")
+            Timber.d("-->${editHabitViewModel.habit.value!!.performances}")
+
+            editHabitViewModel.updatehabit(Habit(habitId = id,
+                title = binding.textInputEditTextFragmentEditHabitName.text.toString(),
+                description = binding.textInputEditTextFragmentAddHabitDescription.text.toString(),
+                editHabitViewModel.habit.value!!.time,
+                editHabitViewModel.habit.value!!.performances,editHabitViewModel.getFrequency()))
+
+            findNavController().navigate(R.id.action_nav_edithabit_to_nav_home)
+        }
     }
 }
--- feature/trackhabit/src/main/java/com/track/trackhabit/habit/presentation/ui/home/EditHabitViewModel.kt
+++ feature/trackhabit/src/main/java/com/track/trackhabit/habit/presentation/ui/home/EditHabitViewModel.kt
@@ -5,21 +5,36 @@ import androidx.lifecycle.*
 import com.track.common.base.AppDispatchers
 import com.track.trackhabit.habit.domain.entity.Habit
 import com.track.trackhabit.habit.domain.usecase.GetHabitByIdUseCase
+import com.track.trackhabit.habit.domain.usecase.UpdateHabitUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import timber.log.Timber
 import java.lang.IllegalArgumentException
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
 class EditHabitViewModel @Inject constructor(
     private val getHabitByIdUseCase: GetHabitByIdUseCase,
+    private val updateHabitUseCase: UpdateHabitUseCase,
     private val dispatcher: AppDispatchers
 ): ViewModel() {
 
     private val _habit = MediatorLiveData<Habit>()
     val habit: LiveData<Habit> get() = _habit
     private var habitSource: LiveData<Habit> = MutableLiveData()
+
+    var monday = MutableLiveData<Boolean>(true)
+    var tuesday = MutableLiveData<Boolean>(true)
+    var wednesday = MutableLiveData<Boolean>(true)
+    var thursday = MutableLiveData<Boolean>(true)
+    var friday = MutableLiveData<Boolean>(true)
+    var saturday = MutableLiveData<Boolean>(true)
+    var sunday = MutableLiveData<Boolean>(true)
+    var time = MutableLiveData<String>()
+
 
     fun getHabit(id: Int){
         viewModelScope.launch(dispatcher.main) {
@@ -30,6 +45,14 @@ class EditHabitViewModel @Inject constructor(
             try {
                 _habit.addSource(habitSource){
                     _habit.value = it
+                    monday.value = _habit.value!!.frequency!![0] == '1'
+                    tuesday.value = _habit.value!!.frequency!![1] == '1'
+                    wednesday.value = _habit.value!!.frequency!![2] == '1'
+                    thursday.value = _habit.value!!.frequency!![3] == '1'
+                    friday.value = _habit.value!!.frequency!![4] == '1'
+                    saturday.value = _habit.value!!.frequency!![5] == '1'
+                    sunday.value = _habit.value!!.frequency!![6] == '1'
+                    time.value = SimpleDateFormat("HH:mm").format(_habit.value!!.time)
                 }
 
             }catch (e: IllegalArgumentException){
@@ -37,5 +60,43 @@ class EditHabitViewModel @Inject constructor(
             }
 
         }
+    }
+
+
+
+    fun updatehabit(habit: Habit){
+        viewModelScope.launch(dispatcher.main) {
+            withContext(dispatcher.io){
+                updateHabitUseCase(habit)
+                Timber.d("-->${habit}_viewmodel")
+                Timber.d("successUpdate")
+            }
+        }
+    }
+
+    private fun convertBooleanToInt(bool: MutableLiveData<Boolean>): Int {
+        if(bool.value == null) return 1
+        if(bool.value == true){
+            return 1
+        }
+        return 0
+    }
+
+    fun getFrequency(): String {
+        return "" + convertBooleanToInt(
+            monday
+        ) + convertBooleanToInt(
+            tuesday
+        ) + convertBooleanToInt(
+            wednesday
+        ) + convertBooleanToInt(
+            thursday
+        ) + convertBooleanToInt(
+            friday
+        ) + convertBooleanToInt(
+            saturday
+        ) + convertBooleanToInt(
+            sunday
+        )
     }
 }
