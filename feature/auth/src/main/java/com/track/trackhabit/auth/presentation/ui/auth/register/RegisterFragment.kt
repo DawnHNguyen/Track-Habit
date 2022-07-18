4ab802a02c473217ed3b36d34ce7af86f0410a9c
lenhathuy <huyhuy2899@gmail.com>
2022-07-18 14:10:37 +0700

    code ui sign up and navigation

--- feature/auth/src/main/java/com/track/trackhabit/auth/presentation/ui/auth/register/RegisterFragment.kt
+++ feature/auth/src/main/java/com/track/trackhabit/auth/presentation/ui/auth/register/RegisterFragment.kt
@@ -1,11 +1,12 @@
-package com.track.trackhabit.habit.presentation.ui.auth.register
+package com.track.trackhabit.auth.presentation.ui.auth.register
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.track.trackhabit.habit.databinding.FragmentRegisterBinding
+import com.track.trackhabit.auth.databinding.FragmentRegisterBinding
+
 
 class RegisterFragment: Fragment() {
     private lateinit var binding: FragmentRegisterBinding
--- feature/trackhabit/src/main/java/com/track/trackhabit/habit/presentation/ui/signup/SignUpFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.track.trackhabit.habit.presentation.ui.signup
-
-import androidx.fragment.app.Fragment
-
-class SignUpFragment: Fragment() {
-}
