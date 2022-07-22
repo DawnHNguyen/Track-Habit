4636c8e0b4a2dd11e1c5cf17351d11a59fd6e6ef
Nguyen Hoang Dang <lophocpzo@gmail.com>
2022-07-22 10:54:16 +0700

    TH-67 Code Splash Activity

---
+++ app/src/main/java/com/track/trackhabit/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.track.trackhabit
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.track.trackhabit.auth.presentation.ui.AuthenticationActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        lifecycleScope.launch {
+            delay(600)
+//            val intent = if (Hawk.contains(HawkKey.ACCESS_TOKEN)) Intent(this@SplashActivity, MainActivity::class.java)
+//            else
+               val intent = Intent(this@SplashActivity, AuthenticationActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
