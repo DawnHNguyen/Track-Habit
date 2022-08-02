3a322096907f8c1fb9c206747be319b3adc491cc
lenhathuy <huyhuy2899@gmail.com>
2022-08-02 09:10:21 +0700

    TH-77 add createAt, updateAt, and format extenstion

--- feature/trackhabit/src/main/java/com/track/trackhabit/habit/domain/entity/User.kt
+++ feature/trackhabit/src/main/java/com/track/trackhabit/habit/domain/entity/User.kt
@@ -1,17 +1,26 @@
 package com.track.trackhabit.habit.domain.entity
 
 import com.track.common.base.DomainModel
+import com.track.common.base.utils.DISPLAY_DATE_FORMAT
+import com.track.common.base.utils.toFormattedString
 import com.track.trackhabit.habit.domain.entity.local.UserLocal
 import com.track.trackhabit.habit.domain.entity.remote.UserDto
+import java.util.*
 
 data class User(
     val userId: String,
     val name: String,
-    val habits: List<Habit>
+    val habits: List<Habit>,
+    val createAt: Date,
+    val updateAt: Date
 ) : DomainModel {
 
-    override fun toLocalDto() = UserLocal(userId, name)
+    override fun toLocalDto() = UserLocal(userId, name, createAt.time, updateAt.time)
 
-    override fun toRemoteDto() = UserDto(userId, name, habits)
+    override fun toRemoteDto() = UserDto(userId,
+        name,
+        habits,
+        createAt.toFormattedString(DISPLAY_DATE_FORMAT),
+        updateAt.toFormattedString(DISPLAY_DATE_FORMAT))
 
 }
--- feature/trackhabit/src/main/java/com/track/trackhabit/habit/domain/entity/local/UserLocal.kt
+++ feature/trackhabit/src/main/java/com/track/trackhabit/habit/domain/entity/local/UserLocal.kt
@@ -4,16 +4,33 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.track.common.base.dto.LocalDto
+import com.track.common.base.utils.DISPLAY_DATE_FORMAT
+import com.track.common.base.utils.toFormattedString
 import com.track.trackhabit.habit.domain.entity.User
 import com.track.trackhabit.habit.domain.entity.remote.UserDto
+import java.util.*
 
 @Entity
 data class UserLocal(
     @PrimaryKey
     @ColumnInfo(name = "user_id") val userId: String,
     val name: String,
+    val createAt: Long,
+    val updateAt: Long
 ) : LocalDto {
-    override fun mapToDomainModel() = User(userId, name, emptyList())
+    override fun mapToDomainModel() = User(
+        userId,
+        name,
+        emptyList(),
+        Date().apply { time = createAt },
+        Date().apply { time = updateAt }
+    )
 
-    override fun mapToRemoteDto() = UserDto(userId, name, emptyList())
+    override fun mapToRemoteDto() = UserDto(
+        userId,
+        name,
+        emptyList(),
+        Date().apply { time = createAt }.toFormattedString(DISPLAY_DATE_FORMAT),
+        Date().apply { time = updateAt }.toFormattedString(DISPLAY_DATE_FORMAT)
+    )
 }
